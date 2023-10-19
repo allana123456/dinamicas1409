7a12f93cded25a8edcfd2b61e82163836a1d25df
allana123456 <115155133+allana123456@users.noreply.github.com>
2023-10-19 09:07:17 -0300

    Add files via upload

---
+++ dinamicas1409-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,25 @@
+function tocaSom (idElementAudio) {
+ document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas =document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while(contador< listaDeTeclas.length){
+
+    listaDeTeclas[contador].onclick = function() {
+     tocaSom('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
+
+
+
+
+    
